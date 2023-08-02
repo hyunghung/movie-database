39dd144f48df4897013c1d8eab3b03d914fcf997
hyunghung <97567582+hyunghung@users.noreply.github.com>
2023-08-02 13:11:20 -0700

    update

---
+++ db/schema.sql
@@ -0,0 +1,13 @@
+DROP DATABASE IF EXISTS movie_db;
+CREATE DATABASE movie_db;
+
+USE movie_db;
+
+CREATE TABLE movies (
+  id INT NOT NULL AUTO_INCREMENT PRIMARY KEY,
+  movie_name VARCHAR(255) NOT NULL,
+);
+
+
+
+
